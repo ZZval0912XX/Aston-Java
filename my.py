9b741e9e95325279c59b779295de506819919e45
ZZval0912XX <valyazakharova87@mail.ru>
2025-04-20 12:52:19 +0300

    Update my.py

--- my.py
+++ my.py
@@ -1,3 +1,28 @@
 print("Valentina")
 print("Вторая строка")
 print("Третья строка")
+
+
+print("Привет", "мир", "!")
+
+print(55, 65,5.8)
+print(True, False)
+
+print("4", "5", "6", "7", sep="-")
+
+a = 17
+print(a)
+a = b = f = 7
+print(a, b, f)
+a -= 1
+print(a)
+print(5/2)
+print(5//2)
+print(5 * 2)
+print(5**2)
+
+
+
+
+
+
