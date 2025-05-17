ff6e59d879484ec81f6292629cebe8544866c3c0
ZZval0912XX <valyazakharova87@mail.ru>
2025-05-17 16:46:32 +0300

    Update my.py

--- practice-if-else.py
+++ practice-if-else.py
@@ -4,6 +4,7 @@
 # nums = [a,b,c]
 # nums. sort()
 # print("Числа в порядке возрастания: ", nums[0],nums[1],nums[2])
+from calendar import month
 
 # sales1=float(input("Введите уровень продаж первого менеджера: "))
 # sales2=float(input("Введите уровень продаж второго менеджера: "))
@@ -39,6 +40,40 @@
 #    print(f"Зарплата 2 менеджера: {salary2} ")
 #    print(f"Зарплата 3 менеджера: {salary3} ")
 #    print(f"Лучший менеджер: {best} - ый ")
+
+# year=int(input("Введите год: "))
+# month=int(input("Введите номер месяца(1-12): "))
+# if month==2:
+#    if (year % 4==0 and year % 100!=0) or (year % 400==0):
+#       print("29 дней")
+#   else:
+#        print("28 дней")
+# elif month in [1,3,5,7,8,10,12]:
+#    print("31 день")
+# elif month in [4,6,9,11]:
+#    print("30 дней")
+# else:
+#    print("Некорректный номер месяца")
+
+
+age=int(input("Введите возраст: "))
+time=int(input("Ввдите время сеанса в часах, например 14: "))
+price=0
+if age<3:
+    price=0
+elif age<=12:
+    price=10
+else:
+    price=15
+    if time<=12 and price>0:
+        price * 0.8
+        print(f"Стоимость билета и оплата:{price} ")16
+
+
+
+
+
+
 
 
 
