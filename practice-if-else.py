80cd8e3c8b5f20d67991e15d2ac4ea4e34a90077
ZZval0912XX <valyazakharova87@mail.ru>
2025-05-17 16:25:43 +0300

    Update my.py

--- practice-if-else.py
+++ practice-if-else.py
@@ -8,36 +8,37 @@
 # sales1=float(input("Введите уровень продаж первого менеджера: "))
 # sales2=float(input("Введите уровень продаж второго менеджера: "))
 # sales3=float(input("Введите уровень продаж третьего менеджера: "))
-if sales1 <=500:
-    salary1 =200+sales1*0.03
-elif sales1 <= 1000:
-    salary1 = 200 + sales1 * 0.05
-else:
-    sales1=200+sales1*0.08
-if sales2 <=500:
-    salary2 =200+sales2*0.03
-elif sales2 <= 1000:
-    salary2 = 200 + sales2 * 0.05
-else:
-    sales2=200+sales2*0.08
-if sales3 <=500:
-    salary3 =200+sales3*0.03
-elif sales3 <= 1000:
-    salary3 =200+sales3*0.05
-else: sales3=200+sales3*0.08
-best_salary=max(salary1,salary2,salary3)
-if best_salary == salary1:
-    salary1+200
-    best =1
-elif (best_salary == salary2):
-    salary2 +=200
-    best = 2
-else:
-    salary3 +=200
-    best =3
-    print(f"Зарплата 1 менеджера: {salary1} ")
-    print(f"Зарплата 2 менеджера: {salary2} ")
-    print(f"Зарплата 3 менеджера: {salary3} ")
-    print(f"Лучший менеджер: {best} - ый ")
+# if sales1 <=500:
+#   salary1 =200+sales1*0.03
+# elif sales1 <= 1000:
+#    salary1 = 200 + sales1 * 0.05
+# else:
+#   sales1=200+sales1*0.08
+# if sales2 <=500:
+#    salary2 =200+sales2*0.03
+# elif sales2 <= 1000:
+#   salary2 = 200 + sales2 * 0.05
+# else:
+#   sales2=200+sales2*0.08
+# if sales3 <=500:
+#    salary3 =200+sales3*0.03
+# elif sales3 <= 1000:
+#    salary3 =200+sales3*0.05
+# else: sales3=200+sales3*0.08
+# best_salary=max(salary1,salary2,salary3)
+# if best_salary == salary1:
+#   salary1+200
+#    best =1
+# elif (best_salary == salary2):
+#    salary2 +=200
+#    best = 2
+# else:
+#   salary3 +=200
+#    best =3
+#   print(f"Зарплата 1 менеджера: {salary1} ")
+#    print(f"Зарплата 2 менеджера: {salary2} ")
+#    print(f"Зарплата 3 менеджера: {salary3} ")
+#    print(f"Лучший менеджер: {best} - ый ")
+
 
 
