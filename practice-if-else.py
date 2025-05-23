650a0f6ef50746432829503259e36edb86eb4c0d
ZZval0912XX <valyazakharova87@mail.ru>
2025-05-23 17:34:21 +0300

    Update my.py

--- practice-if-else.py
+++ practice-if-else.py
@@ -1,73 +1,50 @@
-# a=int(input("Введите первое число: "))
-# b=int(input("Введите второе число: "))
-# c=int(input("Введите третье число: "))
-# nums = [a,b,c]
-# nums. sort()
-# print("Числа в порядке возрастания: ", nums[0],nums[1],nums[2])
-from calendar import month
-
-# sales1=float(input("Введите уровень продаж первого менеджера: "))
-# sales2=float(input("Введите уровень продаж второго менеджера: "))
-# sales3=float(input("Введите уровень продаж третьего менеджера: "))
-# if sales1 <=500:
-#   salary1 =200+sales1*0.03
-# elif sales1 <= 1000:
-#    salary1 = 200 + sales1 * 0.05
-# else:
-#   sales1=200+sales1*0.08
-# if sales2 <=500:
-#    salary2 =200+sales2*0.03
-# elif sales2 <= 1000:
-#   salary2 = 200 + sales2 * 0.05
-# else:
-#   sales2=200+sales2*0.08
-# if sales3 <=500:
-#    salary3 =200+sales3*0.03
-# elif sales3 <= 1000:
-#    salary3 =200+sales3*0.05
-# else: sales3=200+sales3*0.08
-# best_salary=max(salary1,salary2,salary3)
-# if best_salary == salary1:
-#   salary1+200
-#    best =1
-# elif (best_salary == salary2):
-#    salary2 +=200
-#    best = 2
-# else:
-#   salary3 +=200
-#    best =3
-#   print(f"Зарплата 1 менеджера: {salary1} ")
-#    print(f"Зарплата 2 менеджера: {salary2} ")
-#    print(f"Зарплата 3 менеджера: {salary3} ")
-#    print(f"Лучший менеджер: {best} - ый ")
-
-# year=int(input("Введите год: "))
-# month=int(input("Введите номер месяца(1-12): "))
-# if month==2:
-#    if (year % 4==0 and year % 100!=0) or (year % 400==0):
-#       print("29 дней")
-#   else:
-#        print("28 дней")
-# elif month in [1,3,5,7,8,10,12]:
-#    print("31 день")
-# elif month in [4,6,9,11]:
-#    print("30 дней")
-# else:
-#    print("Некорректный номер месяца")
 
 
-age=int(input("Введите возраст: "))
-time=int(input("Ввдите время сеанса в часах, например 14: "))
-price=0
-if age<3:
-   price=0
-elif age<=12:
-   price=10
-else:
-   price=15
-if time <= 12 and price > 0:
-     price*0.8
-print(f"Стоимость билета и оплата:{price} ")
+#n_string = input("Сколько чисел Фибоначчи хотите получить= ")
+#n = int(n_string)
+#fib0 = 0
+#fib1 = 1
+#fib = 0
+#i = 0
+#while i < n:
+#    fib = fib0+fib1
+#    print(fib)
+#    fib0 = fib1
+#    fib1 = fib
+#    i = i+1
+
+
+#for i in range(10,0,-1):
+#    print(i)
+
+
+#input_string = input("Введите строку: ")
+#input_string = input_string.lower()
+#vowels = 0
+#for char in input_string:
+#    if char.isalpha():
+#        if char in 'аоиеуы':
+#            vowels +=1
+#            print(f"Количество гласных букв: {vowels}")
+
+
+#number = 91
+#sum_digits = 0
+#for i in str(number):
+#    sum_digits +=int(i)
+#    print(f"Сумма цифр числа: {sum_digits}")
+
+
+#for i in range(1,11):
+#    for j in range(1,11):
+#        print(i*j, end='\t')
+
+
+
+
+
+
+
 
 
 
