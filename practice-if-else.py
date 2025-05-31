0a333bde2d8a9534678259e045cba3642f222d6b
ZZval0912XX <valyazakharova87@mail.ru>
2025-05-31 13:24:48 +0300

    Update my.py

--- practice-if-else.py
+++ practice-if-else.py
@@ -38,6 +38,25 @@
 #    for j in range(1,11):
 #        print(i*j, end='\t')
 
+#letters = 'ЫгВЫоЯСремДШНККАыкЩЙФа'
+#result = ''
+#for letter in letters:
+#    if letter.islower():
+#        result += letter
+#        print(result)
+
+secret_list = ["Мавпродош", "Лорнектиф", "Древерол", "Фиригарпиг", "Клодобродыч"]
+while True:
+    nickname = input("Для входа введите ваш никнейм: ")
+    if nickname in secret_list:
+        print(f"Ты-свой. Приветсвую любезно. {nickname}!")
+        break
+    else:
+        print("Тут ничего нет. Еще есть вопросы?")
+
+
+
+
 
 
 
