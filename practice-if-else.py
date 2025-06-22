ea62769a8c3f0b89b53627f2038db94f5edaa307
ZZval0912XX <valyazakharova87@mail.ru>
2025-06-22 13:08:15 +0300

    Update my.py

--- practice-if-else.py
+++ practice-if-else.py
@@ -277,6 +277,25 @@ from re import search
 #final_score = calculate_final_score(90, 95, 85, 100, 80)
 #print(final_score)
 
+# рекурсивные функции
+#def fibonacci(n):
+#    if n <= 1:
+#        return n
+#    else:
+#        return fibonacci(n -1) + fibonacci(n - 2)
+#    print(fibonacci(0))
+#    print(fibonacci(1))
+#    print(fibonacci(5))
+#    print(fibonacci(10))
+
+
+
+
+
+
+
+
+
 
 
 
